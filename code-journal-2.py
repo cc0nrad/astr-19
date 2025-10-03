ad34df4522fe6a065fc0c277a73a20734d5d2acc
cc0nrad <coelwong@ucsc.edu>
2025-10-03 01:13:58 -0700

    Add files via upload

---
+++ code-journal-2.py
@@ -0,0 +1,27 @@
+float1 = 2.5
+float2 = 4.9
+
+sum_1 = float1 + float2
+
+print("Sum is"), sum_1
+print("Type of sum is:", type(sum_1))
+
+
+
+int1 = 8
+int2 = 5
+
+difference_1 = int1 - int2
+
+print("Sum is"), difference_1
+print("Type of difference is:", type(difference_1))
+
+
+
+float3 = 6.7
+int3 = 4
+
+product_1 = float3 * int3
+
+print("Sum is"), product_1
+print("Type of product is:", type(product_1))
